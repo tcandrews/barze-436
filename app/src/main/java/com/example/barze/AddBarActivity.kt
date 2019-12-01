60ca0f3e6acddcca35deb0b8e6037d936d4405e1
Catelyn Barnes <Cattaline@wireless-10-104-31-233.umd.edu>
2019-12-01 11:08:31 -0500

    fixed some bugs in add bar

--- app/src/main/java/com/example/barze/AddBarActivity.kt
+++ app/src/main/java/com/example/barze/AddBarActivity.kt
@@ -30,29 +30,18 @@ class AddBarActivity : AppCompatActivity() {
 
     private fun addBarReview() {
         val barName = findViewById<EditText>(R.id.name).text.toString()
-        val barId = findViewById<EditText>(R.id.name).text.toString().trim(' ')
         val hours = findViewById<EditText>(R.id.hours).text.toString()
-        val wait = findViewById<EditText>(R.id.wait).text.toString()
-        val intWait = findViewById<EditText>(R.id.wait).text.toString().toInt()
-        val cover = findViewById<EditText>(R.id.cover).text.toString()
-        val flCover = findViewById<EditText>(R.id.cover).text.toString().toFloat()
+        val wait = findViewById<EditText>(R.id.wait).text.toString().toInt()
+        val cover = findViewById<EditText>(R.id.cover).text.toString().toFloat()
         val hh = findViewById<EditText>(R.id.hh).text.toString()
-        val atmosphere = radioGroup.setOnCheckedChangeListener(
-            RadioGroup.OnCheckedChangeListener { group, checkedId ->
-                val radio: RadioButton = findViewById(checkedId)
-                radio.text
-            }).toString()
         val deals = findViewById<EditText>(R.id.deals).text.toString()
         val events = findViewById<EditText>(R.id.events).text.toString()
         val menu = findViewById<EditText>(R.id.menu).text.toString()
+        val atmosphere = findViewById<EditText>(R.id.atm).text.toString()
 
 
-        if (!TextUtils.isEmpty(barName) || !TextUtils.isEmpty(hours) ||
-            !TextUtils.isEmpty(wait) || !TextUtils.isEmpty(cover) ||
-            !TextUtils.isEmpty(hh) || !TextUtils.isEmpty(atmosphere) ||
-            !TextUtils.isEmpty(deals) || !TextUtils.isEmpty(events)
-        ) {
-            val bar = com.example.barze.data.model.Bar(barName, flCover, atmosphere, intWait, deals, events, hh, hours, menu)
+        if (!TextUtils.isEmpty(barName)) {
+            val bar = com.example.barze.data.model.Bar(barName, cover, atmosphere, wait, deals, events, hh, hours, menu)
             db.collection("bars").document().set(bar)
 
             val intent = Intent(applicationContext, MainActivity::class.java)
